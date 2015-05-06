489549f34e2cde792d742794ac793b2186e4867b
Ricardo Metring <ricardometring@gmail.com>
2015-05-06 15:30:57 -0300

    Arrumado navbar e login

---
+++ static/js/scripts.js
@@ -0,0 +1,30 @@
+function pegarLogin() {
+	return !!(document.cookie.indexOf("login") >= 0);
+}
+
+function logar() {
+	document.cookie = 'login=1;';
+	location.href = 'index.html';
+}
+
+function deslogar() {
+	document.cookie = 'login=0;expires=Thu, 01 Jan 1970 00:00:01 GMT';
+	location.href = 'index.html';
+}
+
+function login() {
+	if (document.getElementById('email').value == 'admin@admin.com' && document.getElementById('senha').value == '123') {
+		console.log("Login");
+		logar();
+	}
+	else {
+		console.error("Erro");
+	}
+	return false;
+}
+
+function enterLogin(e) {
+	if (e.keyCode == 13) {
+		login();
+	}
+}
