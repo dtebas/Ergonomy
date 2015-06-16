79b22c50d5ca7889df657b8e810c7dbaa8d07d79
DanielPedernera <daniel7pp@hotmail.es>
2015-06-16 09:57:46 -0300

    cadastro com mensagem de erro

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -27,8 +27,7 @@ function enterLogin(e) {
 		login();
 	}
 }
-function limpar()
-{
+function limpar(){
 	var nome = document.getElementById('nome');
 	nome.value = "";
 	var sobrenome = document.getElementById('sobrenome');
@@ -41,3 +40,38 @@ function limpar()
 	pass2.value = ""; 
 }
 
+
+function voltar() {
+	var pagina="index.html"
+	if(confirm("Desejavoltar para página inicial?"))
+		location.href=pagina
+} 
+
+function mensagemErroCadastro(){
+	var nome = document.getElementById('nome');
+	var sobrenome = document.getElementById('sobrenome');
+	var email = document.getElementById('email');
+	var pass = document.getElementById('senha');
+	var pass2 = document.getElementById('senhaconf');
+	var mensagem = "";
+	if(nome.value == "")
+		mensagem += "'Nome' é obrigatório.";
+	if(sobrenome.value == "")
+		mensagem += "\n'Sobrenome' é obrigatório.";
+	if(email.value == "")
+		mensagem += "\n'E-mail' é obrigatório.";
+	if(pass.value == "")
+		mensagem += "\n'Senha' é obrigatória";
+	if(pass2.value == "")
+		mensagem += "\n'Confirmação da senha' é obrigatória";
+	
+	if(mensagem != "")
+		alert(mensagem);
+	else
+		mostrar();
+}
+//setTimeout ("voltar()", 20000);*/
+function mostrar(){
+	document.getElementById('confirmation_cadastro').style.display = 'block'
+	document.getElementById('container_cadastro').style.display ='none';
+}
